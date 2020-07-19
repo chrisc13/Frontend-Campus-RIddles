16cf53b7fca2586ab59c674d5451cdbf31b31082
Sergio Rodriguez <srodri82@sundevils.asu.edu>
2020-07-18 18:45:38 -0700

    added service and model

---
+++ angular-CampusRiddles/src/app/_models/riddle.model.ts
@@ -0,0 +1,10 @@
+export class Riddle {
+    id: number;
+    title: string;
+    difficulty: number;
+    prize: string;
+    riddlerName: string;
+    levels: number;
+    riddleDescription: string;
+    // add Riddler Class Object here 
+}
--- angular-CampusRiddles/src/app/create-riddle-page/create-riddle-page.component.ts
+++ angular-CampusRiddles/src/app/create-riddle-page/create-riddle-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import {catchError, retry } from 'rxjs/operators';
+//import { Injectable } from '@angular/core';
+
 
 @Component({
   selector: 'app-create-riddle-page',
