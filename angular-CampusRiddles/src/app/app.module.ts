e2b253ff1acf5332f281e018a3e58ac228121028
Sergio Rodriguez <giorodriguez3898@gmail.com>
2021-11-17 21:27:06 -0700

    finish remodeled

--- angular-CampusRiddles/src/app/app.module.ts
+++ angular-CampusRiddles/src/app/app.module.ts
@@ -6,13 +6,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgxWebstorageModule } from 'ngx-webstorage';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './common/footer/footer.component';
 import { CommunityPageComponent } from './community-page/community-page.component';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
-import { MyRiddlesPageComponent } from './my-riddles-page/my-riddles-page.component';
+import { ProfilePageComponent } from './views/profile-page/profile-page.component';
 import { AuthComponent } from './auth/auth.component';
-import { CreateRiddlePageComponent } from './create-riddle-page/create-riddle-page.component';
-import { HeaderComponent } from './header/header.component';
+
+import { HeaderComponent } from './common/header/header.component';
 import { RiddleService } from './services/riddle.service';
 import { CommunitySubmitPageComponent } from './community-submit-page/community-submit-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -42,9 +41,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     FooterComponent,
     CommunityPageComponent,
     ProfilePageComponent,
-    MyRiddlesPageComponent,
     AuthComponent,
-    CreateRiddlePageComponent,
     HeaderComponent,
     CommunitySubmitPageComponent,
     PageNotFoundComponent,
